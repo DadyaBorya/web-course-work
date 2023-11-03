fab8d8af800d37789cd047cb68b8e020bc2609e1
yappa <julijaogr@ukr.net>
2023-11-03 14:07:35 +0200

    add dockerize

--- server/config/database.js
+++ server/config/database.js
@@ -8,8 +8,11 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT
-    }
+        port: process.env.DB_PORT,
+        retry: {
+            max: 10,
+        }
+    },
 );
 
 module.exports = sequelize;
