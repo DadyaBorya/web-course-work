4a7eca41ca33b234ff410bed453fb7a7b951d906
yappa <julijaogr@ukr.net>
2023-11-03 10:36:38 +0200

    add docker file to server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 require('dotenv').config()
 
 const app = express();
-const port = 5000;
+const port = process.env.SERVER_PORT;
 
 app.use(cors())
 app.use("/uploads", express.static("uploads"))
